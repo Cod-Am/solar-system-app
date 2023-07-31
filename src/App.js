266645cd1c080584a485e0d6ec7762744492c59e
Cod-Am <amrit.672004@gmail.com>
2023-07-31 14:03:31 +0530

    starting from a fresh base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LandingPage from './components/landingpage/LandingPage';
+
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+
     </div>
   );
 }
