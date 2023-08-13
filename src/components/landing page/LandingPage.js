5436541905b40c2c9e8f8b6d0f07388a98ae99e9
Cod-Am <amrit.672004@gmail.com>
2023-08-13 18:30:19 +0530

    done

--- src/components/landing page/LandingPage.js
+++ src/components/landing page/LandingPage.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import './LandingPage.css'
-import background from './img/bg.gif'
+import React, { useState } from 'react';
+import './LandingPage.css';
+import background from './img/bg.gif';
+
 const LandingPage = () => {
+  const [ishover, updateHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    updateHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    // Set a timeout of 2 seconds (2000 milliseconds)
+    setTimeout(() => {
+      updateHover(false);
+    }, 2000);
+  };
+
   return (
     <div className='landingpage'>
-        <img 
-            src={background}
-            alt='not found'
-            className='background'
-        />
-        <div className='landing_entry'>
-          <h1 className='title'>SPACE ODYSSEY</h1>
-          <button className='button'>Get Started</button>
-        </div>
-
+      <img 
+        src={background}
+        alt='not found'
+        className='background'
+      />
+      <div className='landing_entry'>
+        <h1
+          className={`title ${ishover?'transition':'transition'}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          SPACE ODYSSEY
+        </h1>
+        <button className={`button ${ishover ? 'buttonanimationon' : 'buttonanimationoff'}`}>Get Started</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
